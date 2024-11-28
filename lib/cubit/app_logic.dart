7fec3c07ee44a5bbedd0d3318324e9c179b7b0d5
Rihan <100439488+DsaRihan@users.noreply.github.com>
2024-11-28 09:10:36 +0530

    Modified

---
+++ lib/cubit/app_logic.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubit.dart';
+import 'package:flutter_cubit/cubit/app_cubit_states.dart';
+import 'package:flutter_cubit/pages/Welcome_page.dart';
+
+class AppcubitLogicState extends StatefulWidget {
+  const AppcubitLogicState({super.key});
+
+  @override
+  State<AppcubitLogicState> createState() => __AppcubitLogicStateState();
+}
+
+class __AppcubitLogicStateState extends State<AppcubitLogicState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<AppCubit,CubitClass>
+      (builder: (context,state){
+        if(state is WelcomeState){
+          return const WelcomePage();
+        }else
+        {
+          return Container();
+        }
+      },),
+    );
+  }
+}
